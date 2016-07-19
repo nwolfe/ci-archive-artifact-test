52c4db9469caeac4b07c970bd76c2c0581ab09ea
Nate Wolfe <nwolfe@puppet.com>
2016-07-19 16:23:18 -0700

    Include source directory in target path

--- post-suite/archive.rb
+++ post-suite/archive.rb
@@ -3,13 +3,19 @@ require 'fileutils'
 step 'Archive files created during tests' do
   archivedir = 'archive/sut-files'
 
-  source = "/tmp/mytest/master.txt"
-  targetdir = "#{archivedir}/#{master}"
+  ## Copy file(s) from master
+  sourcedir = '/tmp/mytest'
+  source = "#{sourcedir}/master.txt"
+  targetdir = "#{archivedir}/#{master}#{sourcedir}"
+
   FileUtils.mkdir_p(targetdir)
   scp_from(master, source, targetdir)
 
-  source = '/tmp/mytest/agent.txt'
-  targetdir = "#{archivedir}/#{agent}"
+  ## Copy file(s) from agent
+  sourcedir = '/tmp/mytest'
+  source = "#{sourcedir}/agent.txt"
+  targetdir = "#{archivedir}/#{agent}#{sourcedir}"
+
   FileUtils.mkdir_p(targetdir)
   scp_from(agent, source, targetdir)
 end
