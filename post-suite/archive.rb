e5eb48b4cf690568e34b42ab2aa8bb9b76d87694
Nate Wolfe <nwolfe@puppet.com>
2016-07-20 12:07:30 -0700

    Test more files

--- post-suite/archive.rb
+++ post-suite/archive.rb
@@ -12,7 +12,9 @@ end
 step 'Archive files created during tests' do
   ## Copy file(s) from master
   archive_file(master, '/tmp/mytest/master.txt')
+  archive_file(master, '/etc/file-on-master.log')
 
   ## Copy file(s) from agent
   archive_file(agent, '/tmp/mytest/agent.txt')
+  archive_file(agent, '/opt/file-on-agent.log')
 end
--- test-suite/mytest.rb
+++ test-suite/mytest.rb
@@ -1,4 +1,6 @@
 step 'Create files to be archived during post-suite' do
   on(master, 'mkdir -p /tmp/mytest && echo master > /tmp/mytest/master.txt')
+  on(master, 'echo $(hostname) > /etc/file-on-master.log')
   on(agent, 'mkdir -p /tmp/mytest && echo agent > /tmp/mytest/agent.txt')
+  on(agent, 'echo $(hostname) > /opt/file-on-agent.log')
 end
