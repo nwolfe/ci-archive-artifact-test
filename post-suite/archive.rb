ff7a43bbeb56018c5ca752722383a642efbba9ad
Nate Wolfe <nwolfe@puppet.com>
2016-07-21 13:50:04 -0700

    Try file globs

--- post-suite/archive.rb
+++ post-suite/archive.rb
@@ -1,40 +1,20 @@
 require 'fileutils'
 
-# Copies the file into the dedicated archive directory so it will be available
-# in the Jenkins job build artifacts. The file will be copied into a directory
-# named after the host it was copied from.
-#
-# Filepath is expected to be an absolute path on the provided host.
-#
-# Example
-#
-#   Archive the PuppetServer log file from the master host (named 'abc123'),
-#   and archive the Puppet agent log file from the agent host (named 'xyz098'):
-#
-#     archive_file(master, '/var/log/puppetlabs/puppetserver/puppetserver.log')
-#     archive_file(agent, '/var/log/puppetlabs/agent/agent.log')
-#
-#   Results in the following files on the test runner (e.g. Jenkins slave):
-#
-#     archive/sut-files/abc123/var/log/puppetlabs/puppetserver/puppetserver.log
-#     archive/sut-files/xyz098/var/log/puppetlabs/agent/agent.log
-#
-def archive_file(host, filepath)
-  # Value hardcoded in ci-job-configs too
-  archiveroot = 'archive/sut-files'
-  filedir = File.dirname(filepath)
-  targetdir = "#{archiveroot}/#{host}#{filedir}"
+def archive_file_from(host, from_path, opts = {}, archive_root = 'archive/sut-files')
+  filedir = File.dirname(from_path)
+  targetdir = File.join(archive_root, "#{host}#{filedir}")
   FileUtils.mkdir_p(targetdir)
-  scp_from(host, filepath, targetdir)
+  scp_from(host, from_path, targetdir, opts)
 end
 
 step 'Archive files created during tests' do
   ## Copy file(s) from master
-  archive_file(master, '/tmp/mytest/master.txt')
-  archive_file(master, '/etc/file-on-master.log')
+  archive_file_from(master, '/tmp/mytest/*')
+  archive_file_from(master, '/etc/file-on-master.log')
 
   ## Copy file(s) from agent
-  archive_file(agent, '/tmp/mytest/agent.txt')
-  archive_file(agent, '/opt/file-on-agent.log')
-  archive_file(agent, '/foo.bar')
+  archive_file_from(agent, '/tmp/mytest/agent.txt')
+  archive_file_from(agent, '/opt/file-on-agent.log')
+  archive_file_from(agent, '/foo.bar')
+  archive_file_from(agent, 'bar')
 end
--- test-suite/mytest.rb
+++ test-suite/mytest.rb
@@ -1,7 +1,11 @@
 step 'Create files to be archived during post-suite' do
   on(master, 'mkdir -p /tmp/mytest && echo master > /tmp/mytest/master.txt')
+  on(master, 'echo master2 > /tmp/mytest/master2.txt')
+  on(master, 'echo master3 > /tmp/mytest/master3.txt')
+  on(master, 'echo master4 > /tmp/mytest/master4.txt')
   on(master, 'echo $(hostname) > /etc/file-on-master.log')
   on(agent, 'mkdir -p /tmp/mytest && echo agent > /tmp/mytest/agent.txt')
   on(agent, 'echo $(hostname) > /opt/file-on-agent.log')
   on(agent, 'echo foo > /foo.bar')
+  on(agent, 'echo bar > bar')
 end
